3f0731b215582aed5de9ba71668f29dff0870cd1
mouad-suuu <moad.menniui@gmail.com>
2023-11-30 16:25:51 +0100

    first task

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+
+/**
+ * binary tree new node creation
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
+
+binary_tree_t *new ;
+new = malloc(sizeof(binary_tree_t));
+
+if (new == NULL)
+    return NULL;
+new->n = value;
+new->parent = parent;
+new->left = NULL;
+new->right = NULL;
+
+return(new);
+}
